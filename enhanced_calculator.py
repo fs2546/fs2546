3a19e3e98be0aae6367d11506d50bad3cc108e22
fs2546 <fangfei.shu@hotmail.com>
2018-03-15 18:52:52 +0800

    new

--- enhanced_calculator.py
+++ enhanced_calculator.py
@@ -1,24 +1,22 @@
-#! -*- coding: utf-8 -*- 
+#! -*- utf-8 -*- 
 import sys
-print(sys.argv)
+l = []
 for arg in sys.argv[1:]:
-    id, income = arg.split(':')
-try: 
-  income = int(income)
-except ValueError:
-  print("Parameter Error")
-  exit()
+  l.append(arg.split(':'))
 
-  threshold=3500
+def f(income):
+  income = int(income)
   pension=0.08*income
   med=0.02*income
   unemployment=0.005*income
   social=0.06*income
-  resid=income-threshold-pension-med-unemployment-social
+  resid=income-pension-med-unemployment-social
  
-  e=int(income)-3500
-  if e <= 0: tax=0.00
-  elif 0<e<=1500: tax=0.03*e
+  e=resid-3500
+  if e <= 0: 
+    tax=0.00
+  elif 0<e<=1500: 
+    tax=0.03*e
   elif 1500<e<=4500: tax=0.10*e-105
   elif 4500<e<=9000: tax=0.20*e-555
   elif 9000<e<=35000: tax=0.25*e-1005
@@ -28,6 +26,7 @@ except ValueError:
     tax=0.45*e-13505
 
   resid2=resid-tax
+  return '{:.2f}'.format(resid2)
 
-  print("{.2f}:{.2f}") % id % resid2
-
+for a, b in l:
+  print('{}:{}'.format(a,f(b)))

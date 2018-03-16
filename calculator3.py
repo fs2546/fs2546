424a5be8b1ce0f6adee02e282feb6082b9cfce27
fs2546 <fangfei.shu@hotmail.com>
2018-03-16 09:21:19 +0800

    calculator3

---
+++ calculator3.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+import sys
+import csv
+
+class ArgError(Exception):
+    pass
+
+class Args:
+  def __init__(self, args):
+    self.args = args
+
+  def __parse_arg(self, arg):
+    try:
+      value = self.args[self.args.index(arg) + 1]
+    except (ValueError, IndexError):
+      value = None
+    return value
+
+  def get_arg(self, arg):
+    value = self.__parse_arg(arg) 
+    if value is None:
+      raise ArgError('Not found arg %s' % arg)
+    return value
+
+class SheBaoConfig:
+  def __init__(self, file):
+    self.jishu_low, self.jishu_high, self.total_rate = self.__parse_config(file)
+  def __parse_config(self,file):
+    rate = 0
+    jishu_low = 0
+    jishu_high = 0
+    with open(file) as f:
+      for line in f:
+        key, value = line.split('=')
+        key = key.strip()
+        try:
+          value = float(value.strip())
+        except ValueError:
+          continue
+        if key == 'JiShuL':
+          jishu_low = value
+        elif key == 'JiShuH':
+          jishu_high = value
+        else:
+          rate += value
+      return jishu_low, jishu_high, rate
+
+class EmployeeData:
+  def __init__(self,file):
+    self.data = self.__parse_file(file)
+
+  def __parse_file(self,file):
+    data=[]
+    for line in open(file):
+      employee_id, income = line.split(',')
+      data.append((int(employee_id), int(income)))
+    return data
+
+  def __iter__(self):
+    return iter(self.data)
+
+
+class Calculator:
+  tax_start = 3500
+  tax_table = [
+    (80000, 0.45, 13505),
+    (55000, 0.35, 5505),
+    (35000, 0.3, 2755),
+    (9000,0.25,1005),
+    (4500, 0.2, 555),
+    (1500, 0.1, 105),
+    (0,0.03,0),
+  ]
+
+  def __init__(self, config):
+    self.config = config
+
+  def calculate(self, data_item):
+    employee_id, income = data_item
+
+    if income < self.config.jishu_low:
+      shebao = self.config.jishu_low * self.config.total_rate
+    elif income > self.config.jishu_high:
+      shebao = self.config.jishu_high * self.config.total_rate
+    else:
+      shebao = income * self.config.total_rate
+      resid_income = income - shebao
+    tax_income = resid_income - self.tax_start
+
+    if tax_income < 0: 
+      tax = 0
+    else:
+      for item in self.tax_table:
+        if tax_income > item[0]:
+          tax = tax_income * item[1] - item[2]
+          break
+    
+    final_income = resid_income - tax
+    return str(employee_id), str(income), '{:.2f}'.format(shebao),'{:.2f}'.format(tax), '{:.2f}'.format(final_income)
+
+class Exporter:
+  def __init__(self,file):
+    self.file = file
+
+  def export(self,data):
+    content=''
+    for item in data:
+      line=','.join(item) + '\n'
+      content += line
+    with open(self.file, 'w') as f:
+      f.write(content)
+
+class Executor:
+  def __init__(self,args): 
+    args = Args(args)
+
+    config = SheBaoConfig(args.get_arg('-c'))
+    self.employee_data = EmployeeData(args.get_arg('-d'))
+    self.exporter = Exporter(args.get_arg('-o'))
+    self.calculator = Calculator(config)
+  def execute(self):
+    results = []
+    for item in self.employee_data:
+      result = self.calculator.calculate(item)
+      results.append(result)
+
+    self.exporter.export(results)
+
+
+if __name__ == '__main__':
+  executor = Executor(sys.argv[1:])
+  executor.execute()
+
+
